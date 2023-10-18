5bb7d18417faded2e6c0807e4a9d7da5f317e7e0
AryamanDhillon <aryamandhillon@gmail.com>
2023-10-18 13:29:58 -0400

    Add and Remove Courses from Course Selection Page

--- application/src/main/kotlin/CourseSelection.kt
+++ application/src/main/kotlin/CourseSelection.kt
@@ -1,8 +1,12 @@
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,18 +15,47 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+data class addedCourses(
+    // CS
+    val subjectCode :String,
+    // 346
+    val catalogNumber :String,
+    // Application Development
+    val title :String,
+)
+
+private val listOfAddedCourses = listOf(
+    addedCourses(
+        subjectCode = "CS",
+        catalogNumber = "346",
+        title = "Application Development"
+    ),
+    addedCourses(
+        subjectCode = "CS",
+        catalogNumber = "240",
+        title = "Data Structures and Data Management"
+    ),
+    addedCourses(
+        subjectCode = "STAT",
+        catalogNumber = "373",
+        title = "Regression and Forecasting Methods in Finance"
+    )
+)
 fun main() = application {
     Window(
         title = "Course Selection",
         onCloseRequest = ::exitApplication
     ) {
         // takes courses and its sections as inputs to display the course page
-        courseSelection()
+        courseSelection(listOfAddedCourses)
     }
 }
 
 @Composable
-fun courseSelection() {
+fun courseSelection(
+    courseList: List<addedCourses>
+) {
+    var selectedCourses = remember { mutableStateListOf<addedCourses>().apply{addAll(courseList)} }
     // sets the page as a column
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
@@ -36,8 +69,77 @@ fun courseSelection() {
             fontSize = 30.sp,
             maxLines = 1
         )
-
+        Row(
+            Modifier.fillMaxWidth().padding(vertical = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "List of Selected Courses"
+            )
+            Button(
+                onClick = {
+                    // Update Calendar View
+                },
+            ) {
+                Text("Update Calendar")
+            }
+        }
+        val n = courseList.size
+        val extra = 5 - n
+        Row {
+            LazyColumn(Modifier.padding(0.dp)) {
+                items(selectedCourses) {
+                    val (code, number, title) = it
+                    val name = "$code $number: $title"
+                    Row(
+                        Modifier.fillMaxWidth().padding(10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(name)
+                        Button(
+                            onClick = {
+                                selectedCourses.remove(it)
+                            }
+                        ) {
+                            Text("Remove from Schedule")
+                        }
+                    }
+                }
+            }
+        }
         Row (
+            Modifier.fillMaxWidth().padding(vertical = 10.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    // need to go to Search function
+                }
+            ) {
+                Text("Add Courses")
+            }
+        }
+            /*
+            Row(
+                Modifier.fillMaxWidth().padding(10.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = {
+
+                    }
+                ) {
+                    Text("Remove from Schedule")
+                }
+            }
+
+             */
+        /*
+        Row(
             Modifier.fillMaxWidth().padding(vertical = 10.dp),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
@@ -118,5 +220,6 @@ fun courseSelection() {
                 Text(wishList)
             }
         }
+        */
     }
 }
