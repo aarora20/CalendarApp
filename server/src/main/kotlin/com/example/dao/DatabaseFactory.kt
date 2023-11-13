3053741102a98b9d97d01b29b8652e33c1b1167d
c689wang <cedhe@v1040-wn-rt-a-112-19.campus-dynamic.uwaterloo.ca>
2023-11-13 14:12:09 -0500

    basic config for cloud sql in GCP and env variables

--- server/src/main/kotlin/com/example/dao/DatabaseFactory.kt
+++ server/src/main/kotlin/com/example/dao/DatabaseFactory.kt
@@ -10,13 +10,21 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.dotenv
 
 object DatabaseFactory {
     fun init() {
+        val dotenv = dotenv()
         val driverClassName = "org.postgresql.Driver"
-        val jdbcURL = "jdbc:postgresql://db:5432/calendarApp?user=postgres"
+        val dbUser = dotenv["DB_USER"]
+        val dbPass = dotenv["DB_PASS"]
+        val dbName = dotenv["DB_NAME"]
+        val instanceConnectionName = dotenv["INSTANCE_CONNECTION_NAME"]
+        val jdbcURL = "jdbc:postgresql:///${dbName}"
 //        val jdbcURL = "jdbc:postgresql://localhost:5432/calendarApp"
-        val database = Database.connect(createHikariDataSource(jdbcURL, driverClassName))
+        val database = Database.connect(createHikariDataSource(jdbcURL, driverClassName, dbUser,
+            dbPass, instanceConnectionName))
 
         transaction(database) {
             // create tables if not already created
@@ -29,10 +37,18 @@ object DatabaseFactory {
 
     private fun createHikariDataSource(
         url: String,
-        driver: String
+        driver: String,
+        user: String,
+        pass: String,
+        instance: String
     ) = HikariDataSource(HikariConfig().apply {
         driverClassName = driver
         jdbcUrl = url
+        username = user
+        password = pass
+        addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
+        addDataSourceProperty("cloudSqlInstance", instance);
+        addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
         maximumPoolSize = 3
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
