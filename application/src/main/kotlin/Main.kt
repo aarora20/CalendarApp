5983e34646aba934fe14598f61c428146ba33e4f
c689wang <cedhe@v1040-wn-rt-a-54-35.campus-dynamic.uwaterloo.ca>
2023-11-29 12:18:14 -0500

    save screen size to user local system

--- application/src/main/kotlin/Main.kt
+++ application/src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import components.landingPage
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.awt.Dimension
+import java.io.File
 
 @Composable
 @Preview
@@ -13,10 +16,51 @@ fun App() {
     landingPage()
 }
 
+@Serializable
+data class SizeWindow(val width: Int, val height: Int)
+
+fun loadWindowSize(): DpSize? {
+    val homeDir = System.getProperty("user.home")
+    val file = File("$homeDir/windowsize.json")
+
+    return if (file.exists()) {
+        try {
+            val jsonString = file.readText()
+
+            val size: SizeWindow = Json.decodeFromString(jsonString)
+            DpSize(size.width.dp, size.height.dp)
+        } catch (e: Exception) {
+            null
+        }
+
+    } else {
+        // Set to null if default size does not exist
+        null
+    }
+}
+
+fun saveWindowSize(size: DpSize) {
+    val homeDir = System.getProperty("user.home")
+    val file = File("$homeDir/windowsize.json")
+
+    val jsonString = Json.encodeToString(SizeWindow(size.width.value.toInt(), size.height.value.toInt()))
+    file.writeText(jsonString)
+}
+
 fun main() = application {
-    val state = rememberWindowState(placement = WindowPlacement.Maximized)
-    Window(onCloseRequest = ::exitApplication, state) {
-        window.minimumSize = Dimension(1700, 900)
+    val size = loadWindowSize()
+    val state = rememberWindowState()
+    if (size != null) {
+        state.size = size
+    } else {
+        state.placement = WindowPlacement.Maximized
+    }
+
+    Window(onCloseRequest = {
+        saveWindowSize(state.size)
+         exitApplication()
+    } , state) {
+        window.minimumSize = Dimension(1000, 700)
         App()
     }
 }
